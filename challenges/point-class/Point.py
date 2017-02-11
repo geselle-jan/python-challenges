4ff27ef9629302fe2f159b13e77516b7141ee462
Jan Geselle <geselle.jan@googlemail.com>
2017-02-11 15:13:27 +0100

    added point-class challenge

---
+++ challenges/point-class/Point.py
@@ -0,0 +1,9 @@
+class Point():
+    def __init__(self, x, y):
+        pass
+
+    def distanceTo(self, point):
+        pass
+
+    def angleTo(self, point):
+        pass
---
+++ challenges/point-class/main.py
@@ -0,0 +1,19 @@
+from Point import Point
+
+x1 = 0
+y1 = 0
+
+x2 = 10
+y2 = 10
+
+p1 = Point(x1, y1)
+p2 = Point(x2, y2)
+
+print 'p1 = (' + str(x1) + ', ' + str(y1) + ')'
+print 'p2 = (' + str(x2) + ', ' + str(y2) + ')'
+print ''
+print 'expected distance between p1 and p2 = 14,14...'
+print 'computed distance between p1 and p2 = ' + str(p1.distanceTo(p2))
+print ''
+print 'expected angle between p1 and p2 = 135'
+print 'computed angle between p1 and p2 = ' + str(p1.angleTo(p2))

3ce0af4cdd2f7db1ae2618f0cbc50eb7ac260afb
Axdecces <axdecces@gmail.com>
2017-03-04 04:08:47 +0100

    length 0 works now

--- solutions/axdecces/leftpad-challenge/main.py
+++ solutions/axdecces/leftpad-challenge/main.py
@@ -1,8 +1,10 @@
 def leftpad(value, length):
     output = str(value).zfill(length)[-length:]
+    if length == 0:
+        output = ''
     return output
-print 'This is the output'
-print leftpad(12323,12)
+print 'This is the output: '
+print leftpad(12323,0)
 
 
 

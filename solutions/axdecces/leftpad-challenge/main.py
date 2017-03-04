3059f9af056a9c5023193e1172a6b6bce681ea76
Axdecces <axdecces@gmail.com>
2017-03-04 03:49:48 +0100

    Better solution for the challenge

--- solutions/axdecces/leftpad-challenge/main.py
+++ solutions/axdecces/leftpad-challenge/main.py
@@ -1,15 +1,8 @@
-from sys import stdout
-from Leftpad import Leftpad
+value = 1238989888
+length = 22
+print 'This is the output: ' +  str(value).zfill(length)[-length:]
 
-formattedNumber = Leftpad('123', '12')
 
-print 'This id the output if I use the easy way out (but it\'s wrong, so it\'s no real solution): '
-print str(formattedNumber.value).zfill(int(formattedNumber.length))
-print ''
-print 'This is the output:'
-
-for item in formattedNumber.output:
-    stdout.write(item)
 
 
 
